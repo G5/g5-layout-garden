bd12812eb6ad8809226964e1005bcdf175042c53
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-10-03 11:04:45 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source :rubygems
+
+gem "rails", "3.2.7"
+gem "pg"
+gem "jquery-rails"
+
+group :assets do
+  gem "sass-rails", "~> 3.2.3"
+  gem "coffee-rails", "~> 3.2.1"
+  gem "uglifier", ">= 1.0.3"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+G5Hub::Application.config.secret_token = '702521edc28222839126af991491cd0ca1952c89222d1a6c0864435f8290d8936d40c027f3def1fbad1d318158ec8ab5c355c4bf866b69ab2788c02acc9ad39f'
