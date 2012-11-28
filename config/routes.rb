fc5e205fc8b899559d4c9610587d12292ab9aecd
Jessica Lynn Suttles <jlsuttles@gmail.com>
2012-11-28 10:58:17 -0800

    removes unused files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 G5LayoutGarden::Application.routes.draw do
-
   get "static_assets"            => "static_assets#index", as: :static_assets
   get "static_assets/:file_name" => "static_assets#show",  as: :layout
-  get "feed/index"
   root to: "static_assets#index"
 end
